6001191f5522be3a6a12000f3b724bc5c513f972
ApexBugFinder <ApexBugFinder@gmail.com>
2016-07-21 15:16:39 -0700

    TODOITEM - properties
    tested

--- app/src/test/java/com/example/orvilleclarke/testfrag/java/com/example/orvilleclarke/unitTesting/DatabaseTodoItemUnitTest.java
+++ app/src/test/java/com/example/orvilleclarke/testfrag/java/com/example/orvilleclarke/unitTesting/DatabaseTodoItemUnitTest.java
@@ -9,6 +9,8 @@ import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 
+import java.util.Date;
+
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.when;
 
@@ -28,10 +30,55 @@ public class DatabaseTodoItemUnitTest {
     public MockitoRule mockitoRule = MockitoJUnit.rule();
 
     @Test
-    public void testQuery() {
+    public void testPropertiesQuery() {
         ToDoItem a = Mockito.mock(ToDoItem.class);
+
+        //ID
         when(a.getId()).thenReturn(3L);
         assertEquals(a.getId(), 3L);
+
+        //NAME
+        when(a.getName()).thenReturn("New TODO");
+        assertEquals(a.getName(), "New TODO");
+
+        //CREATED ON
+        Date testDate = new Date();
+        when(a.getCreatedOnDate()).thenReturn(testDate);
+        assertEquals(a.getCreatedOnDate(), testDate);
+
+        //TODODUE DATE
+        when(a.getToDoDueDate()).thenReturn(testDate);
+        assertEquals(a.getToDoDueDate(), testDate);
+
+        //COMPLETED ON DATE
+        when(a.getToDoCompleted()).thenReturn(testDate);
+        assertEquals(a.getToDoCompleted(), testDate);
+
+        //DESCRIPTION
+        when(a.getDescription()).thenReturn("TODO Description");
+        assertEquals(a.getDescription(), "TODO Description");
+
+        //PRIORITY
+        String LOW = "LOW";
+        String MED = "MED";
+        String HIGH = "HIGH";
+        when(a.getToDoPriority()).thenReturn(LOW);
+        assertEquals(a.getToDoPriority(),LOW);
+
+        when(a.getToDoPriority()).thenReturn(MED);
+        assertEquals(a.getToDoPriority(),MED);
+
+        when(a.getToDoPriority()).thenReturn(HIGH);
+        assertEquals(a.getToDoPriority(),HIGH);
+
+        //Is Due Date set
+        when(a.isDueDateSet()).thenReturn(true);
+        assertEquals(a.isDueDateSet(),true);
+
+        //Is Completed
+        when(a.isCompleted()).thenReturn(false);
+        assertEquals(a.isCompleted(), false);
+
 
     }
 
